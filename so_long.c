136662e5f0ee7b015bc993ac482e4509ac80454f
epinaud <epinaud@student.42.fr>
2024-11-11 07:08:24 +0100

    Added err check for .ber extension;

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: epinaud <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/20 06:06:02 by epinaud           #+#    #+#             */
-/*   Updated: 2024/11/11 04:10:12 by epinaud          ###   ########.fr       */
+/*   Updated: 2024/11/11 07:11:04 by epinaud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,14 @@ int	main(int argc, char *argv[])
 {
 	t_game	solong;
 
+	solong = (t_game){};
 	solong.mlx_state = 0;
 	if (argc < 2)
 		put_err("Missing map (./maps/<map>.ber)", &solong);
-	if (argc > 2)
+	else if (argc > 2)
 		put_err("Too many arguments", &solong);
+	if (!ft_strnstr(&(argv[1][ft_strlen(argv[1])]) - 4, ".ber", 4))
+		put_err("Wrong or missing file extension (.ber)", &solong);
 	if (parse_map(argv[1], &solong))
 		put_err("Failled to parse map", &solong);
 	put_map(&solong.map, &solong);
