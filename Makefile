58f7b9bf94b3f06ad649e09ce9f92a79cd87cfcf
epinaud <epinaud@student.42.fr>
2024-11-11 04:20:39 +0100

    Introduced player_movement.c

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: epinaud <marvin@42.fr>                     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/05/25 16:30:14 by epinaud           #+#    #+#              #
-#    Updated: 2024/11/08 17:15:45 by epinaud          ###   ########.fr        #
+#    Updated: 2024/11/11 04:02:40 by epinaud          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,7 +14,7 @@ EXE = so_long
 
 CC = cc
 
-CFLAGS = -Wall -Wextra -Werror -ggdb3
+CFLAGS = -Wall -Wextra -Werror
 
 INCLUDES = -I. -Ilibft -Ilibft/printf -I/usr/include -Iminilibx
 
@@ -29,7 +29,7 @@ OS_NAME := $(shell uname -s | tr A-Z a-z)
 $(OBJ_DIR)/%.o : %.c
 	$(CC) -c $(CFLAGS) $(INCLUDES) $< -o $@
 
-all: libft $(OBJ_DIR) $(EXE) os
+all: libft $(OBJ_DIR) $(EXE)
 
 $(EXE) :
 	$(CC) $(CFLAGS) $^ -o $@ $(LDLIBS)
@@ -37,8 +37,8 @@ $(EXE) :
 os:
 	@echo $(OS_NAME)
 
-so_long: .obj/so_long.o .obj/map_parsing.o .obj/draw.o .obj/set_hooks.o \
-			.obj/map_rendering.o .obj/so_long_utils.o
+so_long: .obj/so_long.o .obj/map_parsing.o .obj/draw.o .obj/events.o \
+			.obj/map_rendering.o .obj/so_long_utils.o .obj/player_movement.o
 
 libft:
 	@git -C libft pull
---
+++ player_movement.c
@@ -0,0 +1,108 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   player_movement.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: epinaud <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/11 03:37:01 by epinaud           #+#    #+#             */
+/*   Updated: 2024/11/11 03:57:21 by epinaud          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+// static void	put_exit(size_t exit_stance, t_game *solong)
+// {
+// 	render_tile(TILE_ID, solong->map.exit_pos, solong);
+// 	if (!exit_stance)
+// 		render_tile(EXIT_SHUT_ID, solong->map.exit_pos, solong);
+// 	else
+// 		render_tile(EXIT_OPEN_ID, solong->map.exit_pos, solong);
+// }
+
+static void	render_player(t_point pos, size_t cardinal, t_game *solong)
+{
+	render_tile(TILE_ID, pos, solong);
+	render_tile(cardinal, pos, solong);
+}
+
+static t_point	set_player_angle(int keysym, t_point nxt_pos, size_t *cardinal)
+{
+	if (keysym == XK_Right || keysym == XK_d)
+	{
+		*cardinal = CHAR_RGT_ID;
+		nxt_pos.x += 1;
+	}
+	else if (keysym == XK_Left || keysym == XK_a)
+	{
+		*cardinal = CHAR_LFT_ID;
+		nxt_pos.x -= 1;
+	}
+	else if (keysym == XK_Up || keysym == XK_w)
+	{
+		*cardinal = CHAR_BCK_ID;
+		nxt_pos.y -= 1;
+	}
+	else if (keysym == XK_Down || keysym == XK_s)
+	{
+		*cardinal = CHAR_FRT_ID;
+		nxt_pos.y += 1;
+	}
+	return (nxt_pos);
+}
+
+static void	interact_entity(char nxt_entt, t_game *solong)
+{
+	static size_t	items = 0;
+	static size_t	exit_hit = 0;
+
+	if (exit_hit)
+	{
+		render_tile(EXIT_SHUT_ID, solong->map.exit_pos, solong);
+		exit_hit = 0;
+	}
+	if (nxt_entt == 'A')
+		close_game("You just hit an ennemy !\n\nGAME OVER", solong);
+	else if (nxt_entt == 'C')
+	{
+		if (++items == solong->map.count.collectible)
+		{
+			ft_putendl_fd("Got all collectibles bud, run to the EXIT!", 1);
+			render_tile(EXIT_OPEN_ID, solong->map.exit_pos, solong);
+		}
+	}
+	else if (nxt_entt == 'E')
+	{
+		exit_hit++;
+		if (items == solong->map.count.collectible)
+			close_game("Congratz, you just won the game !", solong);
+	}
+}
+
+void	move_player(int keysym, t_point *curpos, t_game *solong)
+{
+	t_point			nxt_pos;
+	char			nxt_entt;
+	static size_t	steps = 0;
+	size_t			orientation;
+
+	nxt_pos = set_player_angle(keysym, *curpos, &orientation);
+	nxt_entt = solong->map.grid[nxt_pos.y][nxt_pos.x];
+	// if (curpos->y != solong->map.exit_pos.y || curpos->x != solong->map.exit_pos.x)
+	// 	render_player(*curpos, orientation, solong);
+	// else
+	render_player(*curpos, orientation, solong);
+	if (nxt_entt == '1')
+		return (ft_putendl_fd("You just hit a wall !", 1));
+	render_steps_count(++steps, solong);
+	ft_printf("You walked : %u tiles\n", steps);
+		render_tile(TILE_ID, *curpos, solong);
+	if (curpos->y != solong->map.exit_pos.y || curpos->x != solong->map.exit_pos.x)
+		solong->map.grid[curpos->y][curpos->x] = '0';
+	if (nxt_entt != 'E')
+		solong->map.grid[nxt_pos.y][nxt_pos.x] = 'P';
+	render_tile(orientation, nxt_pos, solong);
+	interact_entity(nxt_entt, solong);
+	*curpos = nxt_pos;
+}
